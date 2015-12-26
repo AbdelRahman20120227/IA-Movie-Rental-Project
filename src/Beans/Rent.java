4ed3c8412ac293e278fc47a796351441f4df7380
moham <moham@DESKTOP-74J7S7K>
2015-12-26 23:26:40 +0200

    commit

--- src/Beans/Rent.java
+++ src/Beans/Rent.java
@@ -3,15 +3,11 @@ package Beans;
 import java.sql.Date;
 
 public class Rent {
-	private String userId ;
+	private String userId;
 	private Movie movie;
-	private Date startDate ;
+	private Date startDate;
 	private Date endDate;
-	
-	
-	
-	
-	
+
 	public Rent(String userId, Movie movie, Date startDate, Date endDate) {
 		super();
 		this.userId = userId;
@@ -19,29 +15,37 @@ public class Rent {
 		this.startDate = startDate;
 		this.endDate = endDate;
 	}
+
 	public Movie getMovie() {
 		return movie;
 	}
+
 	public void setMovie(Movie movie) {
 		this.movie = movie;
 	}
+
 	public Date getEndDate() {
 		return endDate;
 	}
+
 	public void setEndDate(Date endDate) {
 		this.endDate = endDate;
 	}
+
 	public Date getStartDate() {
 		return startDate;
 	}
+
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
-	} 
-	 
+	}
+
 }
